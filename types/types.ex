3c3f9afc4a3e0aa9e0ac96c4e3ecce2478d948ea
dougtq <douglas.expalves@gmail.com>
2020-12-06 18:31:53 -0300

    learning elixir types

---
+++ types/types.ex
@@ -0,0 +1,21 @@
+defmodule Types do
+    def integer do
+        1 + 1
+    end
+
+    def float do
+        1.3 + 0.5
+    end
+    def bin do
+        0b1010
+    end
+    def hex do
+        0xFF
+    end
+    def oc do
+        0o755
+    end
+    def bool do
+        false == true
+    end
+end
