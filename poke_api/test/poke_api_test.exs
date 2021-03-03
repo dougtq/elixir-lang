9ad5eb2a8545e9123930529a33ae5c27570355d5
Douglas Alves <douglas.alves@blu365.com.br>
2021-03-03 00:38:08 -0300

    adding teste on new routes

--- poke_api/test/poke_api_test.exs
+++ poke_api/test/poke_api_test.exs
@@ -15,4 +15,53 @@ defmodule PokeApiTest do
     assert conn.status == 200
     assert conn.resp_body == "pong"
   end
+
+  test "it returns 200 on /favicon.ico fetch" do
+    # Create a test connection
+    conn = conn(:get, "/favicon.ico")
+
+    # Invoke the plug
+    conn = PokeApi.Router.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert conn.resp_body == ""
+  end
+
+  test "it returns 404 on non existent route fetch" do
+    # Create a test connection
+    conn = conn(:get, "/non_existent")
+
+    # Invoke the plug
+    conn = PokeApi.Router.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 404
+  end
+
+  test "it returns 404 on poke not found" do
+    # Create a test connection
+    conn = conn(:get, "/poke/doug")
+
+    # Invoke the plug
+    conn = PokeApi.Router.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 404
+  end
+
+  test "it returns 200 on poke found" do
+    # Create a test connection
+    conn = conn(:get, "/poke/mewtwo")
+
+    # Invoke the plug
+    conn = PokeApi.Router.call(conn, @opts)
+
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+  end
 end
