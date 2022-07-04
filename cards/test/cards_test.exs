d7ecd336c3d50a54ac3e6d2d48c02cea0885e2d0
Douglas Alves <douglas.expalves@gmail.com>
2022-07-04 09:39:03 -0300

    test contains? and shuffle methods

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -14,14 +14,22 @@ defmodule CardsTest do
   test "shuffle randomizes a deck" do
     deck = Cards.create_deck
     refute deck == Cards.shuffle(deck)
+    refute deck == Cards.shuffle(deck)
   end
 
   test "create a hand of cards of randomic size" do
-
     hand_size = Enum.random(1..40)
-
     hand = Cards.create_hand(hand_size) |> elem(0)
-
     assert length(hand) == hand_size
+  end
+
+  test "check if a deck does not contains a alien of clubs card" do
+    deck = Cards.create_deck
+    refute Cards.contains?(deck, %{suit: :clubs, value: :alien }) == true
+  end
+
+  test "check if a deck contains a queen of clubs" do
+    deck = Cards.create_deck
+    assert Cards.contains?(deck, %{suit: :clubs, value: :queen }) == true
   end
 end
