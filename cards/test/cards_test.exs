ed00f1873c86a5930e2d4d30313398bd974144f3
Douglas Alves <douglas.expalves@gmail.com>
2022-07-08 10:34:18 -0300

    add setup and setup_all

--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,34 +2,41 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  setup_all _ctx do
+    [hello: Cards.hello]
   end
 
-  test "create_deck makes 40 cards" do
-    deck_length = length(Cards.create_deck)
-    assert deck_length == 40
+  setup _ctx do
+    [deck: Cards.create_deck, cards_left: Cards.create_hand(5) |> elem(1)]
   end
 
-  test "shuffle randomizes a deck" do
-    deck = Cards.create_deck
+  test "greets the world", %{hello: hello} do
+    assert hello == :world
+  end
+
+  test "create_deck makes 40 cards", %{deck: deck} do
+    assert length(deck) == 40
+  end
+
+  test "shuffle randomizes a deck", %{deck: deck} do
     refute deck == Cards.shuffle(deck)
     refute deck == Cards.shuffle(deck)
   end
 
-  test "create a hand of cards of randomic size" do
+  test "creates a hand of cards of randomic size" do
     hand_size = Enum.random(1..40)
-    hand = Cards.create_hand(hand_size) |> elem(0)
-    assert length(hand) == hand_size
+    assert length(Cards.create_hand(hand_size) |> elem(0)) == hand_size
   end
 
-  test "check if a deck does not contains a alien of clubs card" do
-    deck = Cards.create_deck
-    refute Cards.contains?(deck, %{suit: :clubs, value: :alien }) == true
+  test "creates a hand of 5 cards and check the number of cards left", %{cards_left: cards_left} do
+    assert length(cards_left) == 35
   end
 
-  test "check if a deck contains a queen of clubs" do
-    deck = Cards.create_deck
-    assert Cards.contains?(deck, %{suit: :clubs, value: :queen }) == true
+  test "check if a deck does not contains a alien of clubs card", %{deck: deck} do
+    refute Cards.contains?(deck, %{suit: :clubs, value: :alien}) == true
+  end
+
+  test "check if a deck contains a queen of clubs", %{deck: deck} do
+    assert Cards.contains?(deck, %{suit: :clubs, value: :queen}) == true
   end
 end
